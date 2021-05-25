d804fdfdf7f8ba7d69eb02d24b8910f167800de4
Andrew McMillion <amcmillion3@gmail.com>
2021-05-25 11:37:56 +0300

    Create book constructor

--- app.js
+++ app.js
@@ -1,34 +1,37 @@
+
+// Buttons and their event listeners
 const newBookButton = document.getElementById("new-book-button");
-const modal = document.querySelector(".modal");
-const body = document.querySelector("body");
-
-let myLibrary = [];
-
-function Book(title, author, pages, read) {
-  this.title = title;
-  this.author = author;
-  this.pages = pages;
-  this.read = read;
-  this.info = function() {
-    return `${title} by ${author}, ${pages} pages, ${read}`;
-  };
-};
-
-function addBookToLibrary() {
-  // do stuff here
-};
+const closeButton = document.querySelector(".close-button");
+const addBookButton = document.getElementById("add-book-button");
+const modal = document.getElementById("modal");
 
 newBookButton.addEventListener("click", () => {
   modal.style.display = "flex";
 });
 
-body.addEventListener("click", (e) => {
-  if(e.target == body) {
-    console.log("Hey you clicked the body");
-  }
-})
+closeButton.addEventListener("click", () => {
+  modal.style.display = "none";
+});
 
-function newBook() {
+addBookButton.addEventListener("click", addBookToLibrary());
 
+
+// Book Constructor
+class Book {
+  constructor(title, author, pages, read) {
+    this.title = modal.title.value;
+    this.author = modal.author.value;
+    this.pages = modal.pages.value + "pages";
+    this.read = modal.read.checked;
+  };
 };
+
+let myLibrary = [];
+
+function addBookToLibrary() {
+  modal.style.display = "none";
+  let newBook = new Book(title, author, pages, read);
+  myLibrary.push(newBook);
+};
+
 
