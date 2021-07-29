9e8d36952ad0d49303bc0134fcc1b52051fe1d90
Andrew McMillion <amcmillion3@gmail.com>
2021-07-29 09:59:24 -0400

    Add function for displaying library array

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const newBookButton = document.getElementById("new-book-button");
 const closeButton = document.querySelector(".close-button");
 const addBookButton = document.getElementById("add-book-button");
 const modal = document.getElementById("modal");
+const bookCardContainer = document.querySelector(".book-card-container");
 
 newBookButton.addEventListener("click", () => {
   modal.style.display = "flex";
@@ -22,21 +23,53 @@ function Book(title, author, pages, read) {
 
 let myLibrary = [];
 
-function addBookToLibrary() {
+function addBookToLibrary(e) {
   e.preventDefault();
   modal.style.display = "none";
-  getInputValues();
-  console.log(getInputValues());
-  let newBook = new Book(titleInputValue, authorInputValue, pagesInputValue, readInputValue);
+  inputValues = {
+    title: document.getElementById("title").value,
+    author: document.getElementById("author").value,
+    pages: document.getElementById("pages").value,
+    read: document.getElementById("read").value
+  };
+  let newBook = new Book(inputValues.title, inputValues.author, inputValues.pages, inputValues.read);
   myLibrary.push(newBook);
-};
-
-function getInputValues() {
-  let titleInputValue = document.getElementById("title").value;
-  let authorInputValue = document.getElementById("author").value;
-  let pagesInputValue = document.getElementById("pages").value;
-  let readInputValue = document.getElementById("read").value;
-  return titleInputValue, authorInputValue, pagesInputValue, readInputValue;
+  displayLibrary(myLibrary);
 };
 
 
+function displayLibrary (myLibrary) {
+  for(let i = 0; i < myLibrary.length; i++) {
+    let bookCard = document.createElement("div");
+    bookCard.classList.add("book-card");
+    
+    let titleDiv = document.createElement("p");
+    titleDiv.classList.add("card-info");
+    titleDiv.innerHTML = `<strong>Title</strong>: ${myLibrary[i].title}`;
+    bookCard.appendChild(titleDiv);
+    
+    let authorDiv = document.createElement("p");
+    authorDiv.classList.add("card-info");
+    authorDiv.innerHTML = `<strong>Author</strong>: ${myLibrary[i].author}`;
+    bookCard.appendChild(authorDiv);
+    
+    let pagesDiv = document.createElement("p");
+    pagesDiv.classList.add("card-info");
+    pagesDiv.innerHTML = `<strong>Pages</strong>: ${myLibrary[i].pages}`;
+    bookCard.appendChild(pagesDiv);
+
+    let readDiv = document.createElement("button");
+    readDiv.classList.add("read");
+    readDiv.setAttribute("type", "submit");
+    readDiv.innerHTML = "Read";
+    bookCard.appendChild(readDiv);
+
+    let removeDiv = document.createElement("button");
+    removeDiv.classList.add("remove");
+    removeDiv.setAttribute("type", "submit");
+    removeDiv.innerHTML = "Remove";
+    bookCard.appendChild(removeDiv);
+
+    bookCardContainer.appendChild(bookCard);
+  };
+};
